b5b0091c475d1a98f92b78d776d43aa804789fc8
Maxim Khon <fjoe@samodelkin.net>
2024-07-21 22:37:53 +0100

    project/go.mod and project/go.sum should not exist: otherwise the project/ directory is not downloaded as a part of nla_framework

--- templates/main.go
+++ templates/main.go
@@ -62,7 +62,10 @@ func ParseTemplates(p types.ProjectType) map[string]*template.Template {
 
 	// project
 	path := currentDir + "/project/"
-	readFiles("project_", "{{", "}}", path+"config.toml", path+"main.go", path+"go.mod", path+"go.sum", path+"docker-compose.yml", path+"docker-compose.dev.yml", path+"restoreDump.sh", path+"deploy.ps1", path+"Dockerfile")
+	readFiles("project_", "{{", "}}", path+"config.toml", path+"main.go",
+		/* path+"go.mod", path+"go.sum", */
+		path+"docker-compose.yml", path+"docker-compose.dev.yml", path+"restoreDump.sh",
+		path+"deploy.ps1", path+"Dockerfile")
 	if p.IsBackupOnYandexDisk() {
 		readFiles("project_", "[[", "]]", path+"deployYandexBackup.ps1")
 	}
